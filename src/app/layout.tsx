871b9057408b3f023bec474adfd6c672e4d53373
guptaRishi00 <rishigupta54066@gmail.com>
2026-02-08 22:49:39 +0530

    feat: added new changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,6 +29,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        {/* Preconnect to third-party origins */}
+        <link rel="preconnect" href="https://www.transparenttextures.com" />
+
         {/* Google Tag Manager */}
         <Script id="google-tag-manager" strategy="afterInteractive">
           {`
